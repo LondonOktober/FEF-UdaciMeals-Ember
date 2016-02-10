6bc15de710413831c203f39d5f6aa317c4c051ad
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-10 14:54:22 -0800

    add ability to change 'active' day

--- app/components/order-tracker.js
+++ app/components/order-tracker.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   orderManager: Ember.inject.service('order-manager'),
   actions: {
+    setDayTo(day) {
+      this.get('orderManager').setSelectedDayTo(day);
+    },
     remove(day, menuCategory) {
       this.get('orderManager').removeMenuOption(day, menuCategory);
     }
